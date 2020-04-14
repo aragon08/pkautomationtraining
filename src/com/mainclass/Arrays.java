8a29e915fec416c772696f9a690528ce96979cde
Aragon <aragon.c08@gmail.com>
2020-04-14 12:40:28 -0500

    ejercicios 4/13/2020

---
+++ src/com/mainclass/Arrays.java
@@ -0,0 +1,10 @@
+package com.mainclass;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/mainclass/MainSwitchTasks.java
+++ src/com/mainclass/MainSwitchTasks.java
@@ -6,24 +6,35 @@ import com.animals.Ostrich;
 import com.enums.Days;
 
 public class MainSwitchTasks {
-	//TODO: 11.- Create the main method
 	
 	
 	public static void main(String[] args) {
-		//TODO: 12.- Create an object from the Ostrich class, its name is Rocky.
 		
-		//TODO: 13.- Assign any day for eat to the Ostrich.
+		Ostrich o = new Ostrich("Rocky");
 		
-		//TODO: 14.- Create a Scanner object
+		o.setDayToEat(Days.JUEVES);
 		
-		//TODO: 15.- Create a boolean variable to control a loop
+		Scanner scan = new Scanner(System.in);
 		
-		//TODO: 16.- Create a loop that asks user to guess the day the Ostrich eats.
-		//Once the user answers correctly, the loop ends.
-		//HINT: The enum classes has a method named valueOf and name.
-		//Use only if statements
-		//Plus: Use try and catch for any possible input errors.
+		boolean control = true;
 		
+		do {
+			System.out.println("ingresa el dia");
+			Days day = null;
+			Days dayYoEat = o.getDayToEat();
+			try {
+				//Ejecuta algun codigo que pueda retornar o generar alguna excepcion
+				day = Days.valueOf(scan.nextLine());
+			}catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+			if(day.equals(dayYoEat)) {
+				System.out.println("si, como el "+ day.name());
+				control = false;
+			}else {
+				System.out.println("no, intentalo otra vez");
+			}
+		}while(control);
 	}
 
 }
